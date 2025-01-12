8b6e17e662e3f17ebc1415c80d768188f98e6914
Jose <142075563+BirdRoad1@users.noreply.github.com>
2025-01-11 21:14:26 -0500

    Add extra scanner functions to header

--- src/scanner.hpp
+++ src/scanner.hpp
@@ -1,7 +1,13 @@
 #pragma once
 #include "database.hpp"
 
+void scanTask(Database *db, int ip);
+
 void runThread(Database *db, long long startIP, long long endIP);
+
+void runThreadVec(Database *db, std::vector<int> ips);
+
+void scanExtraIPs(Database *db, std::vector<int> &extraIPs);
 
 /**
  * Prints scanner progress periodically
