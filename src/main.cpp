3db17dd655df62495ce3b36fcea72de93aa69686
Jose <142075563+BirdRoad1@users.noreply.github.com>
2025-01-19 03:31:11 -0500

    Configurable thread count, ignore SIGPIPE signal

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,28 @@
 #include "database.hpp"
 #include <optional>
 #include <iostream>
+#include <signal.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        std::cout << "Usage: mcpinger <threads>" << std::endl;
+        return 1;
+    }
+
+    char *threadsStr = argv[1];
+    char *endptr;
+    long threads = strtol(threadsStr, &endptr, 10);
+    if (threadsStr == endptr || threads > 10000)
+    {
+        std::cout << "Invalid threads count" << std::endl;
+        return 1;
+    }
+
+    // Ignore SIGPIPE
+    signal(SIGPIPE, SIG_IGN);
+
     Database db;
 
     if (!db.connect())
@@ -21,6 +40,6 @@ int main()
         std::cout << "Failed to setup database: " << err.what() << std::endl;
     }
 
-    startScanner(&db);
+    startScanner(&db, threads);
     return 0;
 }
